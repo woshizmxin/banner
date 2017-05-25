e06782631b0d4a0f8af13c69f0e37e9ad871e017
zhoumao <zhoumao@meituan.com>
2017-05-25 11:42:32 +0800

    first commit.
    Signed-off-by: zhoumao <zhoumao@meituan.com>

---
+++ app/src/main/java/com/marsthink/carouseldemo/Banner.java
@@ -0,0 +1,17 @@
+/*
+ *     Copyright (c) 2016 Meituan Inc.
+ *
+ *     The right to copy, distribute, modify, or otherwise make use
+ *     of this software may be licensed only pursuant to the terms
+ *     of an applicable Meituan license agreement.
+ *
+ */
+
+package com.marsthink.carouseldemo;
+
+/**
+ * Created by zhoumao on 2017/5/25.
+ */
+
+public class Banner  {
+}
